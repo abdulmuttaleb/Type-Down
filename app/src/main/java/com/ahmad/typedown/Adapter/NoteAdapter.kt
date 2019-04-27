266d93fa0fbe071fda056588795b6f8f4d1f7c2f
Saiko <a.m.abdelmottaleb@gmail.com>
2019-04-27 13:51:06 +0200

    - Main Activity layout - Note list item - defining entry point of app - Note Recycler Adapter - Populating recycler view with notes from local database

---
+++ app/src/main/java/com/ahmad/typedown/Adapter/NoteAdapter.kt
@@ -0,0 +1,40 @@
+package com.ahmad.typedown.Adapter
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.ahmad.typedown.Model.Note
+import com.ahmad.typedown.R
+
+class NoteAdapter(var notes:List<Note>): RecyclerView.Adapter<NoteAdapter.NoteViewHolder>(){
+
+
+    fun setAdapterNotes(notes:List<Note>){
+        this.notes = notes
+        notifyDataSetChanged()
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): NoteViewHolder {
+        return NoteViewHolder(LayoutInflater.from(parent.context).inflate(R.layout.item_note, parent, false))
+    }
+
+    override fun getItemCount(): Int {
+        return notes.size
+    }
+
+    override fun onBindViewHolder(holder: NoteViewHolder, position: Int) {
+        val note = notes[position]
+        holder.title.text = note.title
+        holder.priority.text = note.priority.toString()
+        holder.description.text = note.description
+    }
+
+    inner class NoteViewHolder(itemView : View) : RecyclerView.ViewHolder(itemView){
+        var title : TextView = itemView.findViewById(R.id.tv_title)
+        var priority : TextView = itemView.findViewById(R.id.tv_priority)
+        var description : TextView = itemView.findViewById(R.id.tv_description)
+
+    }
+}
--- app/src/main/java/com/ahmad/typedown/MainActivity.kt
+++ app/src/main/java/com/ahmad/typedown/MainActivity.kt
@@ -4,19 +4,32 @@ import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.ahmad.typedown.Adapter.NoteAdapter
 import com.ahmad.typedown.Model.Note
 import com.ahmad.typedown.ViewModel.NoteViewModel
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var noteViewModel:NoteViewModel
+    private lateinit var recyclerView: RecyclerView
+    private lateinit var adapter:NoteAdapter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        recyclerView = findViewById(R.id.rv_main)
+        recyclerView.layoutManager = LinearLayoutManager(this)
+        recyclerView.setHasFixedSize(true)
+
+        adapter = NoteAdapter(arrayListOf())
+        recyclerView.adapter = adapter
+
+
         noteViewModel = ViewModelProviders.of(this).get(NoteViewModel::class.java)
         noteViewModel.getAllNotes().observe(this, Observer {
-            //update RecyclerView
+            adapter.setAdapterNotes(it)
         })
     }
 }
