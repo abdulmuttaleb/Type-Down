a2964434b96f6c117a50c6ae032745612e3e7d06
Saiko <a.m.abdelmottaleb@gmail.com>
2019-04-27 23:50:56 +0200

    - main activity menu - delete all notes functionality

--- app/src/main/java/com/ahmad/typedown/MainActivity.kt
+++ app/src/main/java/com/ahmad/typedown/MainActivity.kt
@@ -3,6 +3,9 @@ package com.ahmad.typedown
 import android.app.Activity
 import android.content.Intent
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import android.widget.AdapterView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.Observer
@@ -84,4 +87,21 @@ class MainActivity : AppCompatActivity() {
             Toast.makeText(this, "Note not saved!", Toast.LENGTH_SHORT).show()
         }
     }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.main_activity_menu,menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem?): Boolean {
+        return when(item?.itemId){
+            R.id.action_delete_all_notes ->{
+                noteViewModel.deleteAllNotes()
+                Toast.makeText(this,"All notes were deleted", Toast.LENGTH_SHORT).show()
+                true
+            }
+            else ->{ false }
+        }
+    }
+
 }
